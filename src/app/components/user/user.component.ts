0c9b39e6cc4dd251f765e67f932eff731ef0cc04
joshua <nwankwo.joshua@gmail.com>
2019-03-23 20:12:36 +1100

    Updated rule for quotemark

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
+  name: string;
+  phoneNumber: string;
+  count: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.name = "Joshua";
+    this.phoneNumber = "8907665566";
   }
 
+  increment() {
+    this.count = this.count + 1;
+  }
+
+  decrement() {
+    this.count = this.count - 1;
+  }
 }
